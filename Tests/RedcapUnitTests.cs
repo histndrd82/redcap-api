38cbc0523815b140733cc46ad468e594122e91b3
Michael Tran <tranpl@outlook.com>
2019-05-12 13:56:50 -0400

    Update RedcapUnitTests.cs
    add unit tests

--- Tests/RedcapUnitTests.cs
+++ Tests/RedcapUnitTests.cs
@@ -1,5 +1,6 @@
 using Moq;
 using Newtonsoft.Json;
+using Redcap;
 using Redcap.Interfaces;
 using Redcap.Models;
 using System.Threading.Tasks;
@@ -29,9 +30,45 @@ namespace Tests
     {
         // FAKE API Token
         private const string _token = "A8E6949EF4380F1111C66D5374E1AE6C";
-
+        #region ExportArmsAsync
         [Fact]
-        public void ExportRedcapVersionAsync_ShouldReturn_RedcapVersion()
+        public async void ExportArmsAsync_WithContentType_ShouldReturn_Arms()
+        {
+            var arms = "[{\"arm_num\":1,\"name\":\"Arm 1\"}]";
+            var mock = new Mock<IRedcapApi>();
+            mock.Setup(x => x.ExportArmsAsync(_token, Content.Arm, ReturnFormat.json, null, OnErrorFormat.json)).Returns(Task.FromResult(arms));
+            var api = mock.Object;
+
+            // Act
+            var result = await api.ExportArmsAsync(_token, Content.Arm);
+
+            // Assert
+            Assert.Contains("arm_num\":1", result);
+
+            // Verify
+            mock.Verify(x => x.ExportArmsAsync(_token, Content.Arm, ReturnFormat.json, null, OnErrorFormat.json), Times.AtLeastOnce());
+        }
+        [Fact]
+        public async void ExportArmsAsync_ShouldReturn_Arms()
+        {
+            var arms = "[{\"arm_num\":1,\"name\":\"Arm 1\"}]";
+            var mock = new Mock<IRedcapApi>();
+            mock.Setup(x=>x.ExportArmsAsync(_token, ReturnFormat.json, null, OnErrorFormat.json)).Returns(Task.FromResult(arms));
+            var api = mock.Object;
+
+            // Act
+            var result = await api.ExportArmsAsync(_token);
+
+            // Assert
+            Assert.Contains("arm_num\":1", result);
+
+            // Verify
+            mock.Verify(x => x.ExportArmsAsync(_token, ReturnFormat.json, null, OnErrorFormat.json), Times.AtLeastOnce());
+        }
+        #endregion ExportArmsAsync
+        #region ExportRedcapVersionAsync
+        [Fact]
+        public async void ExportRedcapVersionAsync_ShouldReturn_RedcapVersion()
         {
             // Arrange
             const string version = "8.10.15";
@@ -40,12 +77,30 @@ namespace Tests
             var api = mock.Object;
 
             // Act
-            var result = api.ExportRedcapVersionAsync(_token, ReturnFormat.json).Result;
+            var result = await api.ExportRedcapVersionAsync(_token, ReturnFormat.json);
 
             // Assert
             Assert.Contains("8.10.15", result);
         }
+        [Theory]
+        [InlineData("8.10.16", "8.10.16")]
+        [InlineData("8.10.17", "8.10.17")]
+        [InlineData("8.10.18", "8.10.18")]
+        public async void ExportRedcapVersionAsync_ShouldReturn_RedcapVersions(string input, string expected)
+        {
+            // Arrange
+            var version = input;
+            var mock = new Mock<IRedcapApi>();
+            mock.Setup(x => x.ExportRedcapVersionAsync(_token, ReturnFormat.json)).Returns(Task.FromResult(version));
+            var api = mock.Object;
 
+            // Act
+            var result = await api.ExportRedcapVersionAsync(_token, ReturnFormat.json);
+
+            // Assert
+            Assert.Contains(expected, result);
+        }
+        #endregion ExportRedcapVersionAsync
 
     }
 }
