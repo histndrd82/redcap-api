d5a5ae39a2da1ccf9b2b0382b4afcb9807586254
Michael Tran <tranpl@outlook.com>
2019-11-25 07:12:13 -0500

    Update ReturnContent.cs

--- RedcapApi/Models/ReturnContent.cs
+++ RedcapApi/Models/ReturnContent.cs
@@ -2,8 +2,10 @@
 {
     /// <summary>
     /// The content that the response object should contain from Redcap API.
-    /// ReturnContent, 0 = ids
-    /// ReturnContent, 1 = count
+    /// <list type="none">
+    ///     <item>ReturnContent => 0 = ids</item>
+    ///     <item>ReturnContent => 1 = count</item>
+    /// </list>
     /// </summary>
     public enum ReturnContent
     {
